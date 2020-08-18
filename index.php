64b93b4f15f3c03159ec6d6bc6798c4dc5cda001
AIdjIO <ai.dj@pm.me>
2020-08-18 09:36:44 +0100

    updated index.php

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
 <?php 
-
+$f_name="Ai"; //variable with string datatype in double quotes (single quotes can be used too).
+$l_name='Dj';
+$age=44; //integer
+$height=1.91; //float
+$can_vote=true; //boolean
+$address=array('street' => '1 Hacker Way', 'city'=> 'Palo Alto');
+$state = NULL; //variable has no value;
+define('PI',3.14159265);
 ?>
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
-<meta charset="UTF-8"?
+<meta charset="UTF-8">
 <title>PHP Tutorial</title>
 </head>
 <body>
   <p>Name: <?php echo $f_name . ' ' . $l_name; ?></p>
-  
-  ?>
 </body>
 </html>
